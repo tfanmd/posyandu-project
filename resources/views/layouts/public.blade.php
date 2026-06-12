268162f9af2d22ee3e7056ba6abf5bb530abbf86
Ahmad Taufan Hidayat <ahmdtaufan122004@gmail.com>
2026-06-13 01:27:09 +0700

    menambahkan detail nama

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Posyandu Melati</title>
+    <title>Posyandu Sangkuriang</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
@@ -21,7 +21,7 @@
     <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3">
         <div class="container">
             <a class="navbar-brand fw-bold text-green" href="/">
-                <i class="fas fa-heartbeat text-info me-2"></i> Posyandu Melati
+                <i class="fas fa-heartbeat text-info me-2"></i> Posyandu Sangkuriang 
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
